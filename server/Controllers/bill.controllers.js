a17dc80ce7a101533103c2b0a6533285a8106739
Boo <boobalan-mca23>
2025-09-15 16:42:43 +0530

    billUi

--- server/Controllers/bill.controllers.js
+++ server/Controllers/bill.controllers.js
@@ -1,12 +1,22 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const addRawGold=require('../Utils/addRawGoldStock')
-const getCustomerBal=require('../Utils/getCustomerBalance')
+const addRawGold = require("../Utils/addRawGoldStock");
+const getCustomerBal = require("../Utils/getCustomerBalance");
 // createBill
 
 const createBill = async (req, res) => {
-  const {date,time,customerId, billTotal,hallMark, orderItems, received,pureBalance,hallmarkBalance} = req.body;
-   console.log('req  body in bill',req.body)
+  const {
+    date,
+    time,
+    customerId,
+    billTotal,
+    hallMark,
+    orderItems,
+    received,
+    pureBalance,
+    hallmarkBalance,
+  } = req.body;
+  console.log("req  body in bill", req.body);
   try {
     const customerExist = await prisma.customer.findUnique({
       where: { id: parseInt(customerId) },
@@ -21,7 +31,6 @@ const createBill = async (req, res) => {
         .status(400)
         .json({ msg: "At least one order item is required" });
     }
-  
 
     const modifiyOrders = orderItems.map((item, _) => ({
       productName: item.productName,
@@ -34,22 +43,33 @@ const createBill = async (req, res) => {
 
     const newBill = await prisma.bill.create({
       data: {
-       
         date: new Date(date),
         time: time,
         customer_id: parseInt(customerId),
         billAmount: parseFloat(billTotal),
-        hallMark:parseFloat(hallMark),
+        hallMark: parseFloat(hallMark),
         orders: { create: modifiyOrders },
-       },
+      },
       include: {
         orders: true,
         customers: true,
-        
       },
     });
-    // newbill time we need to move rawgold stock  
-    await addRawGold.moveToRawGoldStock(received,newBill.id,customerId) 
+    // newbill time we need to move rawgold stock
+    await addRawGold.moveToRawGoldStock(received, newBill.id, customerId);
+
+    for (const item of orderItems) {
+      if (item.stockId) {
+        await prisma.productStock.update({
+          where: { id: parseInt(item.stockId) },
+          data: {
+            itemWeight: {
+              decrement: parseFloat(item.finalWeight), // reduce stock
+            },
+          },
+        });
+      }
+    }
 
     await prisma.customerBillBalance.upsert({
       where: { customer_id: parseInt(customerId) },
@@ -61,7 +81,6 @@ const createBill = async (req, res) => {
         customer_id: parseInt(customerId),
         balance: parseFloat(pureBalance),
         hallMarkBal: parseFloat(hallmarkBalance),
-
       },
     });
     res
@@ -74,11 +93,11 @@ const createBill = async (req, res) => {
 };
 // update Bill
 
-  const updateBill = async (req, res) => {
-  const { customerId,billId } = req.params;
-  const {received,pureBalance,hallmarkBalance} = req.body;
-  
-  console.log('customerId',customerId)
+const updateBill = async (req, res) => {
+  const { customerId, billId } = req.params;
+  const { received, pureBalance, hallmarkBalance } = req.body;
+
+  console.log("customerId", customerId);
   try {
     // check customer
     const customerExist = await prisma.customer.findUnique({
@@ -95,9 +114,8 @@ const createBill = async (req, res) => {
         .json({ msg: "At least one received item is required" });
     }
     // update bill time we need to create or update rawGoldStock
-     addRawGold.moveToRawGoldStock(received,billId,customerId) 
-   
-   
+    addRawGold.moveToRawGoldStock(received, billId, customerId);
+
     await prisma.customerBillBalance.upsert({
       where: { customer_id: parseInt(customerId) },
       update: {
@@ -121,78 +139,83 @@ const createBill = async (req, res) => {
 const getBillByCustomer = async (req, res) => {
   const { customerId } = req.params;
 
-      try{
-        const customerExist = await prisma.customer.findUnique({
-           where: { id: parseInt(customerId) },
-        });
-  if (!customerExist) {
-    return res.status(400).json({ msg: "Invalid Customer Id" });
+  try {
+    const customerExist = await prisma.customer.findUnique({
+      where: { id: parseInt(customerId) },
+    });
+    if (!customerExist) {
+      return res.status(400).json({ msg: "Invalid Customer Id" });
+    }
+    const bill = await prisma.bill.findMany({
+      where: {
+        customer_id: parseInt(customerId),
+      },
+      include: {
+        orders: true,
+        billReceive: true,
+      },
+    });
+
+    return res.status(200).json({ bill });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ err: err.message });
   }
-      const bill = await prisma.bill.findMany({
-             where: {
-            customer_id: parseInt(customerId),
-            },
-            include:{
-                orders:true,
-                billReceive:true
-            }
-       });
-
-       return res.status(200).json({bill})
-
-     }catch(err){
-        console.error(err.message);
-        return res.status(500).json({ err: err.message });
-      }
 };
 
 // get bill by billID
-    const getBillById=async (req,res) =>{
-        const { billId } = req.params;
-     try{
-        const allBills=await prisma.bill.findMany({
-            where:{
-                id:parseInt(billId)
-            },
-            include:{
-                orders:true,
-                billReceive:true
-            }
-        })
-        return res.status(200).json({allBills})
-     }catch(err){
-        console.error(err.message);
-        return res.status(500).json({ err: err.message });
-     }
+const getBillById = async (req, res) => {
+  const { billId } = req.params;
+  try {
+    const allBills = await prisma.bill.findMany({
+      where: {
+        id: parseInt(billId),
+      },
+      include: {
+        orders: true,
+        billReceive: true,
+      },
+    });
+    return res.status(200).json({ allBills });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ err: err.message });
   }
-
+};
 
 // get All bill
 
-  const geAllBill=async (req,res) =>{
-     try{
-        const allBills=await prisma.bill.findMany({
-            include:{
-                orders:true,
-                billReceive:true,
-                customers:true
-            }
-        })
-        return res.status(200).json({allBills})
-     }catch(err){
-        console.error(err.message);
-        return res.status(500).json({ err: err.message });
-     }
-  }
+const geAllBill = async (req, res) => {
+  try {
+    const allBills = await prisma.bill.findMany({
+      include: {
+        orders: true,
+        billReceive: true,
+        customers:{
+          include:{
+            customerBillBalance:true
+          }
+        },
+      },
+    });
+    const billId=allBills.length===0 ? 1:allBills[allBills.length-1].id
 
-  // customer report controller
-   const customerReport=async(req,res)=>{
-      try {
-        const {customerId}=req.params
-        const { fromDate, toDate } = req.query;
-      
+    return res.status(200).json({ data:allBills,billId:billId+1 });
+
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ err: err.message });
+  }
+};
+
+// customer report controller
+const customerReport = async (req, res) => {
+  try {
+    const { customerId } = req.params;
+    const { fromDate, toDate } = req.query;
+
     const billWhere = {};
-    const billReceiveWhere={};
+    const billReceiveWhere = {};
     const receiptWhere = {};
 
     // If date range is provided
@@ -201,15 +224,15 @@ const getBillByCustomer = async (req, res) => {
       const to = new Date(toDate);
       to.setHours(23, 59, 59, 999); // Include full day
 
-      billWhere.createdAt  = {
+      billWhere.createdAt = {
         gte: from,
         lte: to,
       };
-      billReceiveWhere.createdAt ={
+      billReceiveWhere.createdAt = {
         gte: from,
         lte: to,
-      }
-      receiptWhere.createdAt  = {
+      };
+      receiptWhere.createdAt = {
         gte: from,
         lte: to,
       };
@@ -217,48 +240,48 @@ const getBillByCustomer = async (req, res) => {
 
     // If customer_id is provided
     if (!isNaN(parseInt(customerId))) {
-       billWhere.customer_id = parseInt(customerId);
-       billReceiveWhere.cutomer_id=parseInt(customerId);
-       receiptWhere.customer_id = parseInt(customerId);
-}
+      billWhere.customer_id = parseInt(customerId);
+      billReceiveWhere.cutomer_id = parseInt(customerId);
+      receiptWhere.customer_id = parseInt(customerId);
+    }
 
-    let combinedData=[]
+    let combinedData = [];
     // Check if at least date or customer_id is provided
-    if (fromDate && toDate || customerId) {
+    if ((fromDate && toDate) || customerId) {
       const allBill = await prisma.bill.findMany({
         where: billWhere,
         include: {
-            orders: true,
+          orders: true,
         },
       });
-       const billReceived = await prisma.billReceived.findMany({
+      const billReceived = await prisma.billReceived.findMany({
         where: billWhere,
       });
-       console.log('customer bill ',customerId,allBill)
-       console.log('customer receive',customerId,billReceived)
+      console.log("customer bill ", customerId, allBill);
+      console.log("customer receive", customerId, billReceived);
       const receipt = await prisma.receiptVoucher.findMany({
         where: receiptWhere,
       });
 
-      const allReceive=[...billReceived,...receipt]
-      console.log('All bill report',allBill)
-      console.log('All Receive',allReceive)
+      const allReceive = [...billReceived, ...receipt];
+      console.log("All bill report", allBill);
+      console.log("All Receive", allReceive);
 
-       combinedData = [
+      combinedData = [
         ...allBill.map((bill) => ({
           type: "bill",
           info: bill,
         })),
         ...allReceive.map((receive) => ({
-          type: receive.billId?"billReceive":"ReceiptVoucher",
+          type: receive.billId ? "billReceive" : "ReceiptVoucher",
           info: receive,
         })),
       ];
 
       // get overAll balance
-      const overallBal=await getCustomerBal.getCustomerBalance(customerId)
+      const overallBal = await getCustomerBal.getCustomerBalance(customerId);
 
-      res.status(200).json({data:combinedData,overallBal:overallBal});
+      res.status(200).json({ data: combinedData, overallBal: overallBal });
     } else {
       res.status(200).json(combinedData);
     }
@@ -266,12 +289,12 @@ const getBillByCustomer = async (req, res) => {
     console.error(error);
     res.status(500).json({ error: "Something went wrong" });
   }
-   }
+};
 module.exports = {
   createBill,
   updateBill,
   getBillByCustomer,
   geAllBill,
   getBillById,
-  customerReport
+  customerReport,
 };
