416fa3e9e16b1834d3686e1fe70c127538eeb92f
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-10-13 16:15:30 +0530

    Update productStock.routes.js

--- server/Routes/productStock.routes.js
+++ server/Routes/productStock.routes.js
@@ -1,9 +1,8 @@
 const express=require('express')
-
 const router=express.Router()
-
 const productStock=require('../Controllers/productStock.controller')
 
-router.get('/',productStock.getAllProductStock)
+router.get('/',productStock.getAllProductStock);
+router.put('/:id',productStock.updateProductStock);
 
 module.exports=router
